a87278fa42f4ba7be2a23702e2a3f13eef1ef31f
David Thompson <dthompson2@worcester.edu>
2014-04-17 05:06:03 -0400

    Add bash script for building all firmwares.

---
+++ open software/firmwares/KADE miniArcade/sources/make_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FIRMWARE_DIRS=(
+    'kade-gen'
+    'kade-icade'
+    'kade-key-custom'
+    'kade-keymouse'
+    'kade-led-demo'
+    'kade-mame'
+    'kade-mame-2p'
+    'kade-mame-custom'
+    'kade-mame-extended'
+    'kade-matrix'
+    'kade-pin-custom'
+    'kade-program'
+    'kade-ps3-custom'
+    'kade-psx-custom'
+    'kade-rotary-custom'
+    'kade-wii'
+    'kade-xbox-custom'
+    'tests/minimus'
+)
+working_dir=$(pwd)
+
+for dir in ${FIRMWARE_DIRS[@]}; do
+    cd "$working_dir/$dir"
+    make clean
+    make
+done
